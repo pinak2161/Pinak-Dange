60eb1f2218db16bc18663e818c726e7e983ff677
pinak2161 <pinakdange1621@gmail.com>
2024-12-02 13:44:30 +0530

    techstack  done

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./techstack.css";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
@@ -7,63 +8,82 @@ import {
   DiNodejs,
   DiMongodb,
   DiPython,
-  DiGit,
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
-  SiFirebase,
   SiNextdotjs,
-  SiSolidity,
   SiPostgresql,
+  SiFlask
+
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import { DiDjango } from "react-icons/di";
+import { FaVuejs,FaHtml5,FaCss3Alt  } from "react-icons/fa";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <p>Python</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+        <p>Java</p>
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+        <p>C++</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaHtml5  />
+        <p>Html 5 </p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaCss3Alt />
+        <p>CSS 3</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+        <p>JavaScript</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+        <p>React.js</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+        <p>Node.js</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+        <p>MongoDB</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+        <p>Next.js</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+        <p>PostgreSQL</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+        <p>MySQL</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaVuejs />
+        <p>Vue.js</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlask />
+        <p>Flask</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <DiDjango />
+        <p>Django</p>
+      </Col>
+     
+     
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,31 +1,62 @@
 import React from "react";
+import './techstack.css';
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
   SiSlack,
   SiVercel,
-  SiMacos,
+  SiFirebase
 } from "react-icons/si";
+import { DiGit } from "react-icons/di";
+import {FaFigma , FaGithub ,FaAws,FaDocker  } from "react-icons/fa";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+       <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+        <p>Git</p>
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+      <FaGithub  />
+        <p>Github </p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+        <p>Firebase</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <p>VS Code</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
+        <p>Postman</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
+        <p>Vercel</p>
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaFigma  />
+        <p>Figma </p>
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSlack />
+        <p>Slack</p>
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <FaDocker />
+        <p>Docker</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <FaAws />
+        <p>AWS</p>
+      </Col>
+      
     </Row>
   );
 }
