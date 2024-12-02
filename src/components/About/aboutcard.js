73872d2f37fa1f938f8b948e1d89ad41f02fbb63
pinak2161 <pinakdange1621@gmail.com>
2024-12-02 13:49:11 +0530

    about changes

--- src/components/About/aboutcard.js
+++ src/components/About/aboutcard.js
@@ -8,14 +8,15 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Pinak Dada Rao Dange </span>
+            from <span className="purple"> Pune, Maharashtra, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
+            
+            <div> I am currently pursuing my Bachelor's in Computer Science & Engineering at
+            Savitribai Phule Pune University.
+            </div>
+           
+
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
@@ -34,7 +35,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Pinak Dange</footer>
         </blockquote>
       </Card.Body>
     </Card>
